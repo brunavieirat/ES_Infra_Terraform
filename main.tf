999ad058c4c907624cbfecbe00f4a8167dd73b75
Bruna Vieira <bruna.t@dasa.com.br>
2021-04-28 21:54:54 -0300

    mysql vm

--- main.tf
+++ main.tf
@@ -8,31 +8,104 @@ terraform {
 }
 
 provider "azurerm" {
+  skip_provider_registration = false
   features {}
 }
 
 resource "azurerm_resource_group" "resource" {
-  name     = "example-resources"
-  location = "East US"
+  name     = "myGroup-resources"
+  location = "eastus"
 }
 
-resource "azurerm_mysql_server" "database" {
-  name                = "example-mysqlserver"
-  location            = azurerm_resource_group.resource.location
-  resource_group_name = azurerm_resource_group.resource.name
-
-  administrator_login          = "admin"
-  administrator_login_password = "adminpassword@123"
-
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+resource "azurerm_virtual_network" "main" {
+  name                = "tf-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "tf-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "mydisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "admin"
+    admin_password = "P@ssword123!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+
+resource "azurerm_network_security_group" "secureGroup" {
+  name                = "acceptanceTestSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+   security_rule {
+    name                       = "mysql"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+
+resource "null_resource" "mysql2" {
+  triggers = {
+    order = azurerm_virtual_machine.main.id
+  }
+ 
+
+  provisioner "remote-exec" {
+     connection {
+        type="ssh"
+        user="admin"
+        password="P@ssword123!!"  
+        host = azurerm_public_ip.example.ip_address
+  }
+      inline = [
+      "sudo apt update",
+      "sudo apt-get install -y mysql-server"
+    ]
+  }
+}
+
